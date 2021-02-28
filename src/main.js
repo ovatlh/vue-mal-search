062b89afff4353ceb8bb815f7c97abef0266a4ae
Tavo López Hernández <61797363+ovatlh@users.noreply.github.com>
2021-02-28 14:33:01 -0600

    Axios-VueAxios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import axios from "axios";
+import VueAxios from "vue-axios";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+
+Vue.use(VueAxios, axios);
